b0c4d6a4899203d1f9e62f8a5a582f82be029e76
Jeffrey Hackett <Jeffth92@gmail.com>
2021-07-15 20:58:56 -0400

    claim class added

--- Claim/Claim.cs
+++ Claim/Claim.cs
@@ -8,5 +8,23 @@ namespace Claim
 {
     public class Claim
     {
+        public int ClaimID { get; set; }
+        public string ClaimType { get; set; } //Car, Home, or Theft
+        public string Description { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; } //only for 30 days
+        public Claim() { }
+
+        public Claim(int claimID, string claimType, string description, decimal claimAmount, DateTime dateOfIncedent, DateTime dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncedent;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
     }
 }
--- ClaimREPO/ClaimRepository.cs
+++ ClaimREPO/ClaimRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClaimREPO
 {
-    public class Class1
+    public class ClaimRepository
     {
     }
 }
